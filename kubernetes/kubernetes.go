57ba6f39dcda28bbbc59d56e4f7c8eacabcb7b33
Daisuke Fujita <dtanshi45@gmail.com>
2017-07-14 11:24:24 +0900

    Extract container-repository map from label

--- kubernetes/kubernetes.go
+++ kubernetes/kubernetes.go
@@ -1,9 +1,16 @@
 package kubernetes
 
 import (
+	"strings"
+
+	"github.com/pkg/errors"
 	"k8s.io/client-go/pkg/api/v1"
 	"k8s.io/client-go/pkg/apis/extensions/v1beta1"
 	"k8s.io/client-go/tools/clientcmd"
+)
+
+const (
+	githubLabel = "github"
 )
 
 // DefaultConfigFile returns the default kubeconfig file path
@@ -25,4 +32,24 @@ func ContainerImageFromDeployment(deployment *v1beta1.Deployment, container stri
 	}
 
 	return ""
+}
+
+// RepositoriesFromDeployment returns the reportories attached by 'github' label
+func RepositoriesFromDeployment(deployment *v1beta1.Deployment) (map[string]string, error) {
+	v, ok := deployment.Labels[githubLabel]
+	if !ok {
+		return map[string]string{}, errors.Errorf("label %q not found in deployment %q", githubLabel, deployment.Name)
+	}
+
+	repos := map[string]string{}
+
+	for _, f := range strings.Split(v, ",") {
+		ss := strings.Split(f, ":")
+		if len(ss) != 2 {
+			return map[string]string{}, errors.Errorf(`invalid label %q value %q, must be "container=owner/repo"`, githubLabel, f)
+		}
+		repos[ss[0]] = ss[1]
+	}
+
+	return repos, nil
 }
--- kubernetes/kubernetes_test.go
+++ kubernetes/kubernetes_test.go
@@ -1,6 +1,8 @@
 package kubernetes
 
 import (
+	"reflect"
+	"strings"
 	"testing"
 
 	"k8s.io/client-go/pkg/api/v1"
@@ -44,6 +46,107 @@ func TestContainerImageFromDeployment(t *testing.T) {
 	for _, tc := range testcases {
 		if got := ContainerImageFromDeployment(deployment, tc.container); got != tc.expected {
 			t.Errorf("expected: %q, got: %q", tc.expected, got)
+		}
+	}
+}
+
+func TestRepositoriesFromDeployment(t *testing.T) {
+	testcases := []struct {
+		deployment *v1beta1.Deployment
+		expectErr  bool
+		expected   map[string]string
+		errMsg     string
+	}{
+		{
+			deployment: &v1beta1.Deployment{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "deployment",
+					Namespace: "default",
+					Labels: map[string]string{
+						"github": "rails:rails/rails",
+					},
+				},
+			},
+			expectErr: false,
+			expected: map[string]string{
+				"rails": "rails/rails",
+			},
+		},
+		{
+			deployment: &v1beta1.Deployment{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "deployment",
+					Namespace: "default",
+					Labels: map[string]string{
+						"github": "rails:rails/rails,foobar:dtan4/foobar",
+					},
+				},
+			},
+			expectErr: false,
+			expected: map[string]string{
+				"rails":  "rails/rails",
+				"foobar": "dtan4/foobar",
+			},
+		},
+		{
+			deployment: &v1beta1.Deployment{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "deployment",
+					Namespace: "default",
+					Labels:    map[string]string{},
+				},
+			},
+			expectErr: true,
+			errMsg:    `label "github" not found in deployment "deployment"`,
+		},
+		{
+			deployment: &v1beta1.Deployment{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "deployment",
+					Namespace: "default",
+					Labels: map[string]string{
+						"github": "rails:rails/rails,",
+					},
+				},
+			},
+			expectErr: true,
+			errMsg:    `invalid label "github" value "", must be "container=owner/repo"`,
+		},
+		{
+			deployment: &v1beta1.Deployment{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "deployment",
+					Namespace: "default",
+					Labels: map[string]string{
+						"github": "foobarbaz",
+					},
+				},
+			},
+			expectErr: true,
+			errMsg:    `invalid label "github" value "foobarbaz", must be "container=owner/repo"`,
+		},
+	}
+
+	for _, tc := range testcases {
+		got, err := RepositoriesFromDeployment(tc.deployment)
+
+		if tc.expectErr {
+			if err == nil {
+				t.Errorf("got no error")
+				continue
+			}
+
+			if !strings.Contains(err.Error(), tc.errMsg) {
+				t.Errorf("error %q does not contain %q", err.Error(), tc.errMsg)
+			}
+		} else {
+			if err != nil {
+				t.Errorf("got error: %s", err)
+			}
+
+			if !reflect.DeepEqual(got, tc.expected) {
+				t.Errorf("expected: %q, got: %q", tc.expected, got)
+			}
 		}
 	}
 }
