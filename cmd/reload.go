ab2bcee96d095cf922498d657befcbc8e076ad10
Daisuke Fujita <dtanshi45@gmail.com>
2017-12-05 14:44:02 +0900

    Reload all Deployments unless -d is provided

--- cmd/reload.go
+++ cmd/reload.go
@@ -16,36 +16,42 @@ var reloadCmd = &cobra.Command{
 	RunE:  doReload,
 }
 
+var reloadOpts = struct {
+	deployment string
+	namespace  string
+}{}
+
 func doReload(cmd *cobra.Command, args []string) error {
 	k8sClient, err := kubernetes.NewClient(rootOpts.annotationPrefix, rootOpts.kubeconfig, rootOpts.context)
 	if err != nil {
 		return errors.Wrap(err, "failed to create Kubernetes client")
 	}
 
-	deployments, err := k8sClient.ListDeployments(deployOpts.namespace)
-	if err != nil {
-		return errors.Wrap(err, "failed to retrieve Deployments")
-	}
+	var deployments []*kubernetes.Deployment
 
-	if len(deployments) == 0 {
-		return errors.Errorf("no Deployment found in namespace %s", deployOpts.namespace)
-	}
-
-	targetDeployments := []*kubernetes.Deployment{}
-
-	for _, d := range deployments {
-		if d.IsDeployTarget() {
-			targetDeployments = append(targetDeployments, d)
+	if reloadOpts.deployment == "" {
+		ds, err := k8sClient.ListDeployments(reloadOpts.namespace)
+		if err != nil {
+			return errors.Wrap(err, "failed to retrieve Deployments")
 		}
-	}
 
-	if len(targetDeployments) == 0 {
-		return errors.New("no target Deployments found")
+		if len(ds) == 0 {
+			return errors.Errorf("no Deployment found in namespace %s", reloadOpts.namespace)
+		}
+
+		deployments = ds
+	} else {
+		d, err := k8sClient.GetDeployment(reloadOpts.namespace, reloadOpts.deployment)
+		if err != nil {
+			return errors.Wrapf(err, "failed to retrieve Deployment %s in %s", reloadOpts.deployment, reloadOpts.namespace)
+		}
+
+		deployments = []*kubernetes.Deployment{d}
 	}
 
 	timestamp := time.Now().Local().String()
 
-	for _, d := range targetDeployments {
+	for _, d := range deployments {
 		_, err := k8sClient.ReloadPods(d, timestamp)
 		if err != nil {
 			return errors.Wrap(err, "failed to set annotations")
@@ -59,4 +65,7 @@ func doReload(cmd *cobra.Command, args []string) error {
 
 func init() {
 	RootCmd.AddCommand(reloadCmd)
+
+	reloadCmd.Flags().StringVarP(&reloadOpts.deployment, "deployment", "d", "", "target Deployment")
+	reloadCmd.Flags().StringVarP(&reloadOpts.namespace, "namespace", "n", kubernetes.DefaultNamespace(), "Kubernetes namespace")
 }
